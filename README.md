d015e573787cc72e9957705cf70522e6cb9c360e
mrsarm <mrsarm@gmail.com>
2013-05-29 22:32:38 -0300

    Source code

---
+++ README.md
@@ -0,0 +1,17 @@
+Product Sequence
+================
+
+This module allows to associate a sequence to the product reference.
+The reference (default code) is unique (SQL constraint) and required.
+
+Note: This module is incompatible with nan_product_sequence.
+
+This software was original writed by **Zikzakmedia** for OpenERP 6.x, and
+was patched by **Enterprise Objects Consulting** to be more compatible
+with **OpenERP 6.1**, and fix some issues.
+
+This sources are available in https://github.com/eoconsulting/product_sequence
+
+__________
+
+[Enterprise Objects Consulting](http://www.eoconsulting.com.ar)
---
+++ product_product.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2011 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
+#                  2013 Mariano Ruiz <mrsarm@gmail.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv.orm import Model
+from openerp.osv import fields
+from openerp.tools.translate import _
+
+class product_product(Model):
+    _inherit = 'product.product'
+    _columns = {
+        'default_code' : fields.char('Reference', size=64, required=True),
+        }
+    _sql_constraints = [
+        ('uniq_default_code', 'unique(default_code)', "The reference must be unique"),
+        ]
+    _defaults = {
+        'default_code': lambda * a: '/',
+        }
+
+    def create(self, cr, uid, vals, context=None):
+        if context is None:
+            context = {}
+        if not 'default_code' in vals or vals['default_code'] == '/':
+            vals['default_code'] = self.pool.get('ir.sequence').get(cr, uid, 'product.product')
+        return super(product_product, self).create(cr, uid, vals, context)
+
+    def write(self, cr, uid, ids, vals, context={}):
+        if not isinstance(ids, list):
+            ids = [ids]
+        products_without_code = self.search(cr, uid, [('default_code', 'in', [False, '/']),('id', 'in', ids)], context=context)
+        direct_write_ids = set(ids) - set(products_without_code)
+        super(product_product, self).write(cr, uid, list(direct_write_ids), vals, context)
+        for product_id in products_without_code:
+            vals['default_code'] = self.pool.get('ir.sequence').get(cr, uid, 'product.product')
+            super(product_product, self).write(cr, uid, product_id, vals, context)
+        return True
+
+    def copy(self, cr, uid, id, default={}, context=None):
+        product = self.read(cr, uid, id, ['default_code'], context=context)
+        if  product['default_code']:
+            default.update({
+                'default_code': self.pool.get('ir.sequence').get(cr, uid, 'product.product'),
+            })
+        return super(product_product, self).copy(cr, uid, id, default, context)
